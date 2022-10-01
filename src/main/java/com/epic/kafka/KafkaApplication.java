dd99e2ea379af1008cc2ff0d93dd94ec76b1477b
YasiruLak <yasirudahanayaka19@gmail.com>
2022-10-01 10:42:11 +0530

    Configuration Done

---
+++ src/main/java/com/epic/kafka/KafkaApplication.java
@@ -0,0 +1,13 @@
+package com.epic.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/epic/kafka/KafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epic.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
