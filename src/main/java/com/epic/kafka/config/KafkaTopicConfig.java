87d7483461cf08287540a5df1da25ca6e1c63e98
YasiruLak <yasirudahanayaka19@gmail.com>
2022-10-03 12:36:18 +0530

    create kafka json producer

--- src/main/java/com/epic/kafka/config/KafkaTopicConfig.java
+++ src/main/java/com/epic/kafka/config/KafkaTopicConfig.java
@@ -1,6 +1,7 @@
 package com.epic.kafka.config;
 
 import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.common.config.TopicConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
@@ -10,6 +11,7 @@ public class KafkaTopicConfig {
 
     @Bean
     public NewTopic javaguidesTopic(){
-        return TopicBuilder.name("javaguides").partitions(6).build();
+        return TopicBuilder.name("javaguides").partitions(3).replicas(2)
+                .build();
     }
 }
