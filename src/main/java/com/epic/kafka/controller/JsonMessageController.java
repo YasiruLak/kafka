14644adf9a728a7071eea7bed064433b77825142
YasiruLak <yasirudahanayaka19@gmail.com>
2022-10-03 12:41:13 +0530

    send json message kafka topic

---
+++ src/main/java/com/epic/kafka/controller/JsonMessageController.java
@@ -0,0 +1,24 @@
+package com.epic.kafka.controller;
+
+import com.epic.kafka.kafka.JsonKafkaProducer;
+import com.epic.kafka.kafka.KafkaProducer;
+import com.epic.kafka.payload.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/kafka")
+public class JsonMessageController {
+
+    private JsonKafkaProducer kafkaProducer;
+
+    public JsonMessageController(JsonKafkaProducer kafkaProducer) {
+        this.kafkaProducer = kafkaProducer;
+    }
+
+    @PostMapping("/publish")
+    public ResponseEntity<String> publish(@RequestBody User user){
+        kafkaProducer.sendMessage(user);
+        return ResponseEntity.ok("Json message send to kafka topic");
+    }
+}
